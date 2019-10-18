61a4d58c08894d36042862be7ed2a82892128449
ZhuQiMing <13812603190@139.com>
2019-10-18 10:59:48 +0800

    dist

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     proxyTable: {
       '/api': {
         target: 'http://api.zhuishushenqi.com',
